f3b09401ddaa173ffd997f47693bd27f8a0cb9af
Jaworski <jaworski@red-sky.pl>
2021-07-14 00:04:23 +0200

    feature: implement local strategy

---
+++ src/auth/authStrategies/LocalStrategy.ts
@@ -0,0 +1,37 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from '../../user/entity/User';
+import { UserProviderInterface } from '../../user/service/UserProviderInterface';
+import { UserProvider } from '../../user/service/UserProvider';
+import { hasNotValue } from '../../common/function/Nullable';
+import { ArgonHashVerifier } from '../services/ArgonHashVerifier';
+import { PasswordVerifier } from '../services/PasswordVerifier';
+import { LOGIN_FIREWALL } from '../../common/const/firewall';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, LOGIN_FIREWALL) {
+  constructor(
+    @Inject(UserProvider) private readonly userProvider: UserProviderInterface,
+    @Inject(ArgonHashVerifier) private readonly passwordVerifier: PasswordVerifier,
+  ) {
+    super({
+      passwordField: 'password',
+      usernameField: 'email',
+    });
+  }
+
+  async validate(username: string, password: string): Promise<User> {
+    const user = await this.userProvider.findByEmail(username);
+    if (hasNotValue(user)) {
+      //....
+    }
+
+    const isPasswordCorrect = await this.passwordVerifier.verify(password, user.passwordHash);
+    if (!isPasswordCorrect) {
+      //....
+    }
+
+    return user;
+  }
+}
