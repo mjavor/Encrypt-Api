0b49db3e351823f33cd053af2bd315e5fbbd621a
Jaworski <jaworski@red-sky.pl>
2021-07-13 23:00:21 +0200

    feature: register config module
    config: add env dist

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthModule,
   ],
   controllers: [],
