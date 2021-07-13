56384606456cce7039c5fa007c76a0e28ca3a444
Jaworski <jaworski@red-sky.pl>
2021-07-14 01:34:35 +0200

    feature: mark auth services as injectable
    feature: implement jwt token generating

--- src/auth/services/ArgonHashVerifier.ts
+++ src/auth/services/ArgonHashVerifier.ts
@@ -1,6 +1,8 @@
 import { PasswordVerifier } from './PasswordVerifier';
 import * as argon from 'argon2';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class ArgonHashVerifier implements PasswordVerifier {
   async verify(phrase: string, hash: string): Promise<boolean> {
     return argon.verify(hash, phrase);
--- src/auth/services/JwtTokenManager.ts
+++ src/auth/services/JwtTokenManager.ts
@@ -1,9 +1,15 @@
 import { User } from '../../user/entity/User';
 import { JwtPayload } from '../dto/JwtPayload';
 import { JwtService } from '@nestjs/jwt';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class JwtTokenManager {
   constructor(private readonly jwtService: JwtService) {}
+
+  generateToken(user: User): Promise<string> {
+    return this.jwtService.signAsync(this.createPayload(user));
+  }
 
   createPayload({ emailAddress }: User): JwtPayload {
     return {
