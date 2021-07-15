5039c2e96bcfab6f9bb606d1d11f5c028acfa15d
Jaworski <jaworski@red-sky.pl>
2021-07-15 19:51:58 +0200

    chore: install helmet and morgan
    feature: enable installed packages

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from './AppModule';
 import { loadFile, loadFixtures } from './common/function/LoadFixtures';
 import { ConfigService } from '@nestjs/config';
 import { PORT } from './common/const/Config';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await loadFixtures(app);
   await loadFile(app);
+
+  app.use(morgan('tiny'));
+  app.use(helmet());
 
   await app.listen(app.get<ConfigService>(ConfigService).get<number>(PORT));
 }
