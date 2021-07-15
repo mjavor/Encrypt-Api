3adc1d507a7646f454cbbceaafec47fec2d54344
Jaworski <jaworski@red-sky.pl>
2021-07-15 18:50:52 +0200

    feature: add running on port specified in env file

--- src/common/const/Config.ts
+++ src/common/const/Config.ts
@@ -1,2 +1,4 @@
 export const JWT_SECRET = 'JWT_SECRET';
 export const JWT_EXPIRES_IN = 'JWT_EXPIRES_IN';
+
+export const PORT = 'PORT';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 import { loadFile, loadFixtures } from './common/function/LoadFixtures';
+import { ConfigService } from '@nestjs/config';
+import { PORT } from './common/const/Config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,6 @@ async function bootstrap() {
   await loadFixtures(app);
   await loadFile(app);
 
-  await app.listen(3000);
+  await app.listen(app.get<ConfigService>(ConfigService).get<number>(PORT));
 }
 bootstrap();
